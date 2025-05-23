4863dea007bab25dd3f546e30e2a515dc788f36f
OLC <ritsukage00@gmail.com>
2025-05-23 16:26:13 +0800

    修正格式

--- Excel2Json/ProcessError.cs
+++ Excel2Json/ProcessError.cs
@@ -1,17 +1,9 @@
 namespace Excel2Json;
 
-public class ProcessError
+public class ProcessError(string message, int row, int column, bool isWarning)
 {
-    public string Message { get; }
-    public int Row { get; }
-    public int Column { get; }
-    public bool IsWarning { get; }
-
-    public ProcessError(string message, int row, int column, bool isWarning)
-    {
-        Message = message;
-        Row = row;
-        Column = column;
-        IsWarning = isWarning;
-    }
+    public string Message { get; } = message;
+    public int Row { get; } = row;
+    public int Column { get; } = column;
+    public bool IsWarning { get; } = isWarning;
 } 
